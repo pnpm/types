b367e14c7a4225707f80b308bd326afcd7a8fecf
zkochan <zoltan.kochan@gmail.com>
2017-11-07 00:52:50 +0200

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+export type Dependencies = {
+  [name: string]: string
+}
+
+export type PackageBin = string | {[commandName: string]: string}
+
+export interface PackageJson {
+  name: string,
+  version: string,
+  private?: boolean,
+  bin?: PackageBin,
+  directories?: {
+    bin?: string,
+  },
+  dependencies?: Dependencies,
+  devDependencies?: Dependencies,
+  optionalDependencies?: Dependencies,
+  peerDependencies?: Dependencies,
+  bundleDependencies?: string[],
+  bundledDependencies?: string[],
+  scripts?: {
+    [name: string]: string
+  },
+  config?: Object,
+  engines?: {
+    node?: string,
+    npm?: string,
+  },
+  cpu?: string[],
+  os?: string[],
+}
+
+// Most of the fields in PackageMetadata are also in PackageJson
+// except the `deprecated` field
+export interface PackageMetadata {
+  name: string,
+  version: string,
+  dependencies?: Dependencies,
+  devDependencies?: Dependencies,
+  optionalDependencies?: Dependencies,
+  peerDependencies?: Dependencies,
+  bundleDependencies?: string[],
+  bundledDependencies?: string[],
+  engines?: {
+    node?: string,
+    npm?: string,
+  },
+  cpu?: string[],
+  os?: string[],
+  deprecated?: string,
+}
